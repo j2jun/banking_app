0cd560aeddca3dbeba1a88cfb2a87217c78214da
Joseph Jun <54129361+j2jun@users.noreply.github.com>
2024-08-20 09:22:36 -0700

    finished file & folder structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ["latin"], variable: '--font-inter' });
 const ibmPlexSerif = IBM_Plex_Serif({ 
   subsets: ["latin"], 
   weight: ['400', '700'],
-  variable: `--font-ibm-plex-serif`
+  variable: '--font-ibm-plex-serif'
 })
 
 export const metadata: Metadata = {
