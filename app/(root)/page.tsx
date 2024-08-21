c646fe551f696a3933fb9c6aca0d1acc142a6ea1
Joseph Jun <54129361+j2jun@users.noreply.github.com>
2024-08-21 09:02:43 -0700

    implement home page UI

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox';
 
 const Home = () => {
+    const loggedIn = { firstName: 'Joseph' };
+
     return (
-        <div>Home</div>
+        <section className="home">
+            <div className="home-content">
+                <header className="home-header">
+                    <HeaderBox
+                        type="greeting"
+                        title="Welcome"
+                        user={loggedIn?.firstName || 'Guest'}
+                        subtext="Access and manage your account and transactions efficiently"
+                    />
+
+                    <TotalBalanceBox
+                        accounts={[]}
+                        totalBanks={1}
+                        totalCurrentBalance={2460.45}
+                    />
+                </header>
+            </div>
+        </section>
     )
 }
 
